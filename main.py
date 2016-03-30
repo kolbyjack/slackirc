8b4d57a72fa56fd0df8cce1da3c1362c93ae0d36
Jon Kolb <kolbyjack@gmail.com>
2016-03-30 09:42:55 -0400

    add persistent dictionary for settings

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
 #!/usr/bin/env python
 
 import irc
+import json
 import time
 import yaml
 
 from slackclient import SlackClient
+
+class Database(dict):
+    def __init__(self, filename):
+        self.filename = filename
+        self.open(filename)
+
+    def open(self, filename):
+        self.clear()
+        try:
+            with open(filename, 'r') as fp:
+                self.update(json.load(fp))
+        except:
+            pass
+
+    def save(self):
+        with open(self.filename, 'w') as fp:
+            json.dump(self, fp)
+
+    def __getitem__(self, key):
+        return self.setdefault(key, {})
+
+    def __setitem__(self, key, value):
+        super(Database, self).__setitem__(key, value)
+        self.save()
+
+    def __delitem__(self, key):
+        super(Database, self).__delitem__(key)
+        self.save()
+
+db = Database('slackirc.json')
 
 with open('config.yml', 'r') as fp:
     config = yaml.load(fp)
@@ -16,19 +47,44 @@ if slack.rtm_connect():
 
         for event in events:
             if event['type'] == 'message':
-                if 'subtype' not in event:
-                    user = slack.server.users.find(event['user'])
-                    print '%s: %s' % (user.name, event['text'])
+                if 'subtype' in event:
+                    continue
+
+                if 'text' not in event:
+                    continue
+
+                user = slack.server.users.find(event.get('user', None))
+                if user is None:
+                    continue
+
+                channel = slack.server.channels.find(event.get('channel', None))
+                if channel is None:
+                    continue
+
+                message = event['text']
+                if channel.id[0] == 'D':
+                    args = message.split(' ')
+                    if len(args) == 3 and args[0] == 'set':
+                        db['users'].setdefault(user.id, {})[args[1]] = args[2]
+                        db.save()
+                        if args[1] == 'nick':
+                            # TODO: set user's nick in irc
+                            pass
+                        elif args[1] == 'password':
+                            # TODO: identify with nickserv
+                            pass
+                    elif len(args) == 2 and args[0] == 'unset':
+                        try:
+                            del db['users'].setdefault(user.id, {})[args[1]]
+                            db.save()
+                        except KeyError:
+                            pass
+                    print '@%s: %s' % (user.name, message)
                 else:
-                    print event
-            elif event['type'] == 'presence_change':
-                # TODO: Set IRC away status
-                pass
-            elif event['type'] not in ('reconnect_url', 'hello', 'user_typing'):
-                print event
+                    print '#%s/%s: %s' % (channel.name, user.name, message)
 
         if len(events) == 0:
-            time.sleep(0.5)
+            time.sleep(0.2)
 
 #[
 #  id : U0VTX1MQE tz : None name : ircbot real_name :,
